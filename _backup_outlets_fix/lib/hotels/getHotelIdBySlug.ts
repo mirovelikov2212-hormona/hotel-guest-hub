b8a99731ee05ec6430677e024fd32b36ca57ad55
Miroslav Velikov <miro.velikov.22.12@gmail.com>
2026-05-20 00:32:09 +0200

    Remove old backup folder

--- _backup_outlets_fix/lib/hotels/getHotelIdBySlug.ts
+++
@@ -1,6 +0,0 @@
-import { getHotelByAnySlug } from "@/lib/hotels/getHotelByAnySlug";
-
-export async function getHotelIdBySlug(inputSlug?: string): Promise<string> {
-  const hotel = await getHotelByAnySlug(inputSlug);
-  return hotel.id;
-}
