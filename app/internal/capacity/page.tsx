f06900a79a678b3110a8dde1aeec29a347842fcb
mirovelikov2212-hormona <miro.velikov.22.12@gmail.com>
2026-09-02 10:43:25 +0200

    cleanup: remove remaining internal capacity page

--- app/internal/capacity/page.tsx
+++
@@ -1,9 +0,0 @@
-import { notFound } from "next/navigation";
-import { CapacityRunner } from "./CapacityRunner";
-
-export const dynamic = "force-dynamic";
-
-export default function CapacityPage() {
-  if (process.env.VERCEL_ENV !== "preview") notFound();
-  return <CapacityRunner />;
-}
