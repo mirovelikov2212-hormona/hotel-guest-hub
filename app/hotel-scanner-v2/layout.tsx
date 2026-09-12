45753220ab0ce374557dcb85d49bd5a1ecf1ddaf
mirovelikov2212-hormona <miro.velikov.22.12@gmail.com>
2026-09-12 10:51:44 +0200

    feat(scanner-v2): add shared light-dark tools theme

---
+++ app/hotel-scanner-v2/layout.tsx
@@ -0,0 +1,5 @@
+import ToolsThemeShell from "@/components/internal-tools/ToolsThemeShell";
+
+export default function HotelScannerV2Layout({ children }: { children: React.ReactNode }) {
+  return <ToolsThemeShell>{children}</ToolsThemeShell>;
+}
