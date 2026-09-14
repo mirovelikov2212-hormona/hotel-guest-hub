2c23a06b52f0f592a939715090f2db700e733761
mirovelikov2212-hormona <miro.velikov.22.12@gmail.com>
2026-09-14 05:30:09 +0200

    feat(scanner-v2): add workflow preview layout

---
+++ app/hotel-scanner-v2-workflow/layout.tsx
@@ -0,0 +1,6 @@
+import ToolsThemeShell from "@/components/internal-tools/ToolsThemeShell";
+import "../hotel-scanner-v2/scanner-v2.css";
+
+export default function HotelScannerV2WorkflowLayout({ children }: { children: React.ReactNode }) {
+  return <ToolsThemeShell>{children}</ToolsThemeShell>;
+}
